ed93ec116714dd23c7c71444e379318887f7ed72
Mark Lapierre <mlapierre@gmail.com>
2016-11-15 06:59:48 -0500

    smtp backend

--- config/emails.js
+++ config/emails.js
@@ -1,13 +1,12 @@
 module.exports = {
     emails: {
-        backend: 'sendgrid',
-        from: 'Josh <josh@getstream.io>',
-        // docs: https://github.com/ghaiklor/sails-service-mailer
-        provider: {
-            auth: {
-                api_user: process.env.SENDGRID_USERNAME,
-                api_key: process.env.SENDGRID_PASSWORD
-            }
-        }
+        backend: 'smtp',
+		provider: {
+			service: 'gmail', // Use's nodemailer's pre-configured settings
+		    auth: { 
+		      user: process.env.EMAIL_USERNAME, 
+		      pass: process.env.EMAIL_PASSWORD, 
+		    }
+		}
     }
 }
